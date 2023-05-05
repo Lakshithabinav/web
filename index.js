9c75ae62f462f0e27a3163b2ccc1efc754f042ea
Lakshithabinav <126949013+Lakshithabinav@users.noreply.github.com>
2023-05-05 15:12:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let packet_list = [];
 var max_voltage = 0;
 var min_voltage=450;
 var avg_packet
+var voltage;
+var val=0;
 async function startSerial() {
   if ("serial" in navigator) {
     try {
@@ -28,20 +30,73 @@ async function startSerial() {
         }
 
         packet = new TextDecoder().decode(value).trim();
-        const voltage = parseFloat(packet);
+        voltage = parseFloat(packet);
+        
         packet_list.push(parseFloat(packet));
         document.getElementById("pac").innerHTML = packet;
+
         
-        //Max-voltage.
-        if ((voltage > max_voltage)&&(voltage<450)) {
-          max_voltage = voltage;
+
+        //send mail if voltage is low
+        if(voltage<10 && val==0){
+          try{
+            console.log("Current Packet: ", packet);
+            console.log("Avg",avg_packet.toFixed(2));
+          
+            var params = {
+              current_voltage: packet,
+              mesage:"Alert ! voltage reached lower the limit",
+              sub:"voltage is low!"
+            };
+            const serviceID = "service_1tl8y6c";
+            const templateID = "template_410ut7o";
+            console.log("alert-mailsent");
+            
+            emailjs.send(serviceID, templateID, params);
+            val=1;
+          }
+          catch{
+            console.log("err");
+          }
         }
-        //Min-voltage.
-        if ((voltage < min_voltage)) {
-          min_voltage = voltage;
+
+        //send mail if voltage if high
+        else if(voltage>400 && val==0 && packet_list.length >= 10){
+          try{
+            console.log("Current Packet: ", packet);
+            console.log("Avg",avg_packet.toFixed(2));
+          
+            var params = {
+              current_voltage: packet,
+              mesage:"Alert ! voltage reached higher the limit",
+              sub:"voltage is high!"
+            };
+            const serviceID = "service_1tl8y6c";
+            const templateID = "template_410ut7o";
+            console.log("alert-mailsent");
+            
+            emailjs.send(serviceID, templateID, params);
+            val=1;
+          }
+          catch{
+            console.log("err");
+          }
         }
+        else{
+          val=1;
+        }
+        
+
         //to display in webpage.
-        if (packet_list.length >= 10) { 
+        if (packet_list.length >= 10) { //Max-voltage.
+          if ((voltage > max_voltage)&&(voltage<450)) {
+            max_voltage = voltage;
+          }
+  
+          //Min-voltage.
+          if ((voltage < min_voltage)) {
+            min_voltage = voltage;
+          }
           avg_packet = packet_list.slice(-10).reduce((a, b) => a + b) / 10;
           document.getElementById("avg").innerHTML = avg_packet.toFixed(2);
           document.getElementById("max").innerHTML = max_voltage;
@@ -57,6 +112,7 @@ async function startSerial() {
     console.log("Web serial not supported");
   }
 }
+
 //to send mail manually
 function send_mail(){
   console.log("Current Packet: ", packet);
@@ -74,6 +130,7 @@ function send_mail(){
   emailjs.send(serviceID, templateID, params)
   alert("Your message sent successfully!!")
 }
+
 //To send mail automatically.
 setInterval(function(){
   console.log("Current Packet: ", packet);
@@ -87,8 +144,12 @@ setInterval(function(){
   };
   const serviceID = "service_1tl8y6c";
   const templateID = "template_lc75t1n";
-  console.log("mailsent");
+  console.log("automatic-mailsent");
   
   emailjs.send(serviceID, templateID, params)
 },100*1000);
+
+
+
+
 
